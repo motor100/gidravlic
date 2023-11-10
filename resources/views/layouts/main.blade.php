8d1ba3265627b1cc2bd97e24848e9b2f2dc960d1
Evgeny <260443@mail.ru>
2023-11-10 16:26:19 +0500

    Metrika evgeniakhalturina

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -501,5 +501,22 @@
 
   <script src="//code.jivo.ru/widget/qXz1GgymN2" async></script>
 
+  <!-- Yandex.Metrika counter evgeniakhalturina@yandex.ru -->
+  <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(95528104, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+    });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/95528104" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
